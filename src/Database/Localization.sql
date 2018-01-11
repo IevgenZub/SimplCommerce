934cb4a0ff8fbcdd32574df0f6ed15f1964e032d
Ievgen Zubkov <euginzub@mail.ru>
2018-01-11 10:10:31 +0200

    Add Ru localization

---
+++ src/Database/Localization.sql
@@ -0,0 +1,22 @@
+﻿INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Departure', N'Откуда')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Landing', N'Куда')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Departure Date', N'Дата вылета')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Landing Date', N'Дата прилета')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Return Date', N'Дата возврата')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'More Details', N'Детали')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Carriers', N'Перевозчик')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Round Trip', N'Туда и обратно')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Travelers', N'Кол-во мест')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'One Way', N'В одну сторону')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Check reservation', N'Мои бронирования')
+
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Reservation Number', N'Номер')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Find Reservation', N'Найти')
+
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Cheapest Tickets', N'Лучшие цены на билеты')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Find cheapest tickets like million people', N'Покупай билеты по самым оптимальным ценам как миллионы других')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Adult', N'Взрослый')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Child', N'Ребенок')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Save', N'Сохранить')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Search results', N'Результаты поиска')
+
