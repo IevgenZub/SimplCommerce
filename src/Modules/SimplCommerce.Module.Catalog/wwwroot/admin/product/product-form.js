2e7e1c2e645ee2f8b5e5ce76d87d195f9be64d4f
Ievgen Zubkov <euginzub@mail.ru>
2018-02-28 09:34:43 +0200

    Fix for departure dates generation

--- src/Modules/SimplCommerce.Module.Catalog/wwwroot/admin/product/product-form.js
+++ src/Modules/SimplCommerce.Module.Catalog/wwwroot/admin/product/product-form.js
@@ -128,7 +128,7 @@
                 var end = new Date(endDate);
 
                 while (current <= end) {
-                    if ($('#' + current.getDay()).prop('checked')) {
+                    if ($('#' + (current.getDay() + 1)).prop('checked')) {
                         retVal.push(new Date(current));
                     }
                     current.setDate(current.getDate() + 1);
