36496c0993cfb19ec3f0c3904c89b2a360efbf97
Ievgen Zubkov <euginzub@mail.ru>
2018-04-26 10:53:29 +0300

    Localization

--- src/Database/Localization.sql
+++ src/Database/Localization.sql
@@ -39,6 +39,38 @@ INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'
 INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Want to stay updated with the best offerings and latest news? Enter your email and click Subscribe', N'Будьте в курсе самых лучших предложений и свежих новостей! Введите свой email и нажмите «Подписаться».')
 INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'About', N'Информация')
 
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Discount', N'Скидка')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Order Total', N'Сумма заказа')
+
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Coupon Code', N'Купон на скидку')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Agency Fee', N'Комиссия агентства')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Back to Search', N'Назад к поиску')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'adult', N'взрослый')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'child', N'ребенок')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'baby', N'младенец')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Passenger Registration', N'Регистрация Пассажиров')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'First Name', N'Имя')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Last Name', N'Фамилия')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Birth Date', N'Дата Рождения')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Document', N'Паспорт')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Phone', N'Телефон')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Country', N'Страна')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Sex', N'Пол')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Flight', N'Рейс')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Continue', N'Продолжить')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Add New Passenger', N'Добавить нового пассажира')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Add Passenger', N'Добавить пассажира')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Select', N'Выберите')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Choose payment method', N'Выберите способ оплаты')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Bank Transfer', N'Банковский перевод')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'passenger(s)', N'пассажира(ов)')
+INSERT [dbo].[Localization_Resource] ([CultureId], [Key], [Value]) VALUES (5, N'Your order has been received!', N'Мы получии Ваш заказ!')
+
+
+
+
+
+
  
 
 
