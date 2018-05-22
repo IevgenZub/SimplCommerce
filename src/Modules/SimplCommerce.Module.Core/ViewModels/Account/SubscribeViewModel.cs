e5e94dd927e064adfcc0ba499a46592f34623578
Ievgen Zubkov <euginzub@mail.ru>
2018-05-22 10:54:54 +0300

    Subscribe email

---
+++ src/Modules/SimplCommerce.Module.Core/ViewModels/Account/SubscribeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplCommerce.Module.Core.ViewModels.Account
+{
+    public class SubscribeViewModel
+    {
+        public string Email { get; set; }
+    }
+}
